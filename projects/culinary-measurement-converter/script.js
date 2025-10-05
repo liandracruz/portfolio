40e03afc4f59d9f03b82b2b0f508253f13539487
liandracruz <liandraluiza.02@gmail.com>
2025-10-05 15:01:36 -0300

    Começando JavaScript

--- projects/culinary-measurement-converter/script.js
+++ projects/culinary-measurement-converter/script.js
@@ -1,10 +1,24 @@
 //Armazenar o ingrediente em uma variável
+const ingrediente = document.getElementById('ingredientes-opcoes');
+let ingredienteSelecionado = '';
+ingrediente.addEventListener('change', function(event) {
+    ingredienteSelecionado = event.target.value;
+    console.log(ingredienteSelecionado);
+}); //O que tem de errado?
 
-//Ajustar as unidades de medida para serem compatíveis com o tipo de ingrediente: deixar ml apenas para ingredientes liquidos
+//Armazenar a unidade inicial em uma variável
+const unidadeInicial = document.getElementById('unidade-inicial');
 
-//Armazenar a quantidade inicial em uma variável
+//Armazenar a quantiade inicial em uma variável
+const quantidade = document.querySelector('.quantidade');
 
 //Armazenar a unidade desejada em uma variável
+const unidadeDesejada = document.getElementById('unidade-desejada');
+
+console.log(ingrediente, unidadeInicial, quantidade, unidadeDesejada);
+
+
+//Ajustar as unidades de medida para serem compatíveis com o tipo de ingrediente: deixar ml apenas para ingredientes liquidos
 
 //Criar um evento: quando o botão "calcular" é clicado a conversão é feita.
 
