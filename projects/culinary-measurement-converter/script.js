71e928e122fc0aca9eb8de97c14d8291e7a5db91
liandracruz <liandraluiza.02@gmail.com>
2025-10-05 13:38:03 -0300

    Planejamento JavaScript

---
+++ projects/culinary-measurement-converter/script.js
@@ -0,0 +1,11 @@
+//Armazenar o ingrediente em uma variável
+
+//Ajustar as unidades de medida para serem compatíveis com o tipo de ingrediente: deixar ml apenas para ingredientes liquidos
+
+//Armazenar a quantidade inicial em uma variável
+
+//Armazenar a unidade desejada em uma variável
+
+//Criar um evento: quando o botão "calcular" é clicado a conversão é feita.
+
+//Exibir o resultado da conversão na div "resultado"
