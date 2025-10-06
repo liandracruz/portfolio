0723a4c8967e2e621e4a15e69fe32dcdb0ddbd16
liandracruz <liandraluiza.02@gmail.com>
2025-10-06 10:20:26 -0300

    JavaScript

--- projects/culinary-measurement-converter/script.js
+++ projects/culinary-measurement-converter/script.js
@@ -4,16 +4,31 @@ let ingredienteSelecionado = '';
 ingrediente.addEventListener('change', function(event) {
     ingredienteSelecionado = event.target.value;
     console.log(ingredienteSelecionado);
-}); //O que tem de errado?
+});
 
 //Armazenar a unidade inicial em uma variável
 const unidadeInicial = document.getElementById('unidade-inicial');
+let uniInicialSelecionada = '';
+unidadeInicial.addEventListener('change', function(event) {
+    uniInicialSelecionada = event.target.value;
+    console.log(uniInicialSelecionada);
+});
 
 //Armazenar a quantiade inicial em uma variável
 const quantidade = document.querySelector('.quantidade');
+let quantidadeInfo = '';
+quantidade.addEventListener('input', function(event) {
+    quantidadeInfo = event.target.value;
+    console.log(quantidadeInfo);
+});
 
 //Armazenar a unidade desejada em uma variável
 const unidadeDesejada = document.getElementById('unidade-desejada');
+let uniDesejadaSelecionada = '';
+unidadeDesejada.addEventListener('change', function(event) {
+    uniDesejadaSelecionada = event.target.value;
+    console.log(uniDesejadaSelecionada);
+});
 
 console.log(ingrediente, unidadeInicial, quantidade, unidadeDesejada);
 
