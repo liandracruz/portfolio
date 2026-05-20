278a6a118ce27bdd59645fd503433c5a855bf974
liandracruz <liandraluiza.02@gmail.com>
2026-05-20 16:26:14 -0300

    creating the card pool

---
+++ projects/memory-game/script.js
@@ -0,0 +1,20 @@
+const cardPool = [
+    {iconClass: "fa-solid fa-chess-king", color: "#6A00F4", name: "chess-king"},
+    {iconClass: "fa-solid fa-guitar", color: "#BC00DD", name: "guitar"},
+    {iconClass: "fa-solid fa-wine-glass", color: "#E500A4", name: "wine-glass"},
+    {iconClass: "fa-solid fa-umbrella", color: "#256EFF", name: "umbrella"},
+    {iconClass: "fa-brands fa-fly", color: "#BE0A3A", name: "fly"},
+    {iconClass: "fa-solid fa-pencil", color: "#F84113", name: "pencil"},
+    {iconClass: "fa-solid fa-rocket", color: "#0424DE", name: "rocket"},
+    {iconClass: "fa-solid fa-paper-plane", color: "#1E2624", name: "paper-plane"},
+    {iconClass: "fa-solid fa-bell", color: "#F8C21F", name: "bell"},
+    {iconClass: "fa-solid fa-apple-whole", color: "#0DB128", name: "apple"},
+    {iconClass: "fa-solid fa-chess-queen", color: "#EE0B4F", name: "chess-queen"},
+    {iconClass: "fa-solid fa-award", color: "#00D161", name: "award"},
+    {iconClass: "fa-brands fa-canadian-maple-leaf", color: "#FB3E3E", name: "maple-leaf"},
+    {iconClass: "fa-solid fa-paw", color: "#FF6593", name: "paw"},
+    {iconClass: "fa-solid fa-ice-cream", color: "#B077C5", name: "ice-cream"},
+    {iconClass: "fa-solid fa-lemon", color: "#8BE95C", name: "lemon"},
+    {iconClass: "fa-solid fa-graduation-cap", color: "#C20044", name: "graduation-cap"},
+    {iconClass: "fa-solid fa-anchor", color: "#24F1DD", name: "anchor"}
+];
